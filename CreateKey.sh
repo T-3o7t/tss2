29fe3e3df59fcae5a852efd7a1f9a52a9545b333
taniguchi <assnsmile2005@gmail.com>
2026-05-21 06:45:25 +0000

    added key

--- CreateKey.sh
+++ CreateKey.sh
@@ -1,8 +1,13 @@
 tpm2 createprimary -C o -c primary.ctx -G rsa2048 -g sha256;
-tpm2 create -C primary.ctx -u child.pub -r child.priv -G rsa2048;
-tpm2 load -C primary.ctx -u child.pub -r child.priv -c child.ctx;
-tpm2_evictcontrol \
-  -C o \
-  -c child.ctx \
-  0x81010001
-tpm2_readpublic -c child.ctx -o pub.pem -f pem
+tpm2 create -C primary.ctx -u hmac.pub -r hmac.priv -G keyedhash ;
+tpm2 load -C primary.ctx -u hmac.pub -r hmac.priv -c hmac.ctx;
+tpm2 evictcontrol -C o -c hmac.ctx 0x81010001;
+
+tpm2 create -C primary.ctx -u sign.pub -r sign.priv -G rsa2048;
+tpm2 load -C primary.ctx -u sign.pub -r sign.priv -c sign.ctx;
+tpm2 evictcontrol -C o -c sign.ctx 0x81010010;
+tpm2 getcap handles-persistent;
+
+tpm2_readpublic -c 0x81010001;
+tpm2_readpublic -c 0x81010010;
+
